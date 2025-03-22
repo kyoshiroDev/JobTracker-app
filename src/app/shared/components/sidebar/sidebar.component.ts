e7eb72ca4f084f74659bffbadeb4cdaa018029fe
Gregory Tahir <tahir.gregory.dev@gmail.com>
2025-03-22 22:30:49 +0100

    fix: correction bug sur la selection du lien dans la side bar

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -4,29 +4,34 @@ import { Sidebarmenu } from '../../models/sidebarmenu';
 
 @Component({
   selector: 'fdw-sidebar',
-  imports: [RouterLink, RouterLinkActive,],
+  imports: [RouterLink, RouterLinkActive],
   template: `
-  <div class="side-bar">
-    <h1 class="text-3xl">JobTracker</h1>
-    <ul>
-      @for(menu of sidebar(); track menu.id){
-        <li class="side-li" routerLinkActive="active-link" [routerLinkActiveOptions]="{ exact: true}">
-          <a routerLink={{menu.routerLink}}>{{ menu.name }}</a>
-        </li>
-      }
-    </ul>
-  </div>
+    <div class="side-bar">
+      <div class="flex flex-col justify-center items-center h-[80px]">
+        <h1 class="text-3xl">JobTracker</h1>
+      </div>
+      <div class="flex flex-col">
+        @for(menu of sidebar(); track menu.id){
+        <a
+          class="side-a"
+          routerLinkActive="active-link"
+          [routerLinkActiveOptions]="{ exact: true }"
+          routerLink="{{ menu.routerLink }}"
+          >{{ menu.name }}</a
+        >
+        }
+      </div>
+    </div>
   `,
 })
 export class SidebarComponent {
+  protected readonly router = inject(Router);
 
-  protected readonly router = inject(Router)
- 
   protected readonly sidebar = signal<Sidebarmenu[]>([
-    {id:1, name: "📊 Dashboard", routerLink:"/"},
-    {id:2, name: "📂 Mes Candidatures", routerLink:"candidatures"},
+    { id: 1, name: '📊 Dashboard', routerLink: '/' },
+    { id: 2, name: '📂 Mes Candidatures', routerLink: 'candidatures' },
     //{id:3, name: "📝 Ma TodoList", routerLink:"todoliste"},
     //{id:4, name: "🔍 Offres d'Emploi", routerLink:"Offres-d-emploi"},
     //{id:5, name: "⚙️ Parametre", routerLink:"setting"},
-  ])
+  ]);
 }
